9155124ea2f4a6737d8e06e972505ddd13b9c57d
hshinkawa <gokukyukyoku555@gmail.com>
2021-12-13 17:22:45 +0900

    saveするdirの変更

--- compare_source_prob.py
+++ compare_source_prob.py
@@ -32,7 +32,15 @@ def main(num_trials, num_selections, avoid_aem, num_arms_min, num_arms_max):
     if not avoid_aem:
         ave_aems = []
         std_aems = []
+    now = datetime.datetime.now()
+    current_time = now.strftime("%Y-%m-%d-%H-%M-%S")
+    dir_for_output = "data/" + current_time + "/"
     for num_arms in range(num_arms_min, num_arms_max+1): # Number of machines = 3,4,5.
+        os.makedirs(dir_for_output+'{}M/conv/'.format(num_arms))
+        os.makedirs(dir_for_output+'{}M/psm/'.format(num_arms))
+        os.makedirs(dir_for_output+'{}M/ideal/'.format(num_arms))
+        if not avoid_aem:
+            os.makedirs(dir_for_output+'{}M/aem/'.format(num_arms))
         env = np.array([0.1]*(num_arms-4) + [0.3, 0.5, 0.7, 0.9])[:num_arms] # Reward envirionment
         envs = list(set(itertools.permutations(env))) # All environments.
         conv_results = np.zeros(len(envs))
@@ -46,19 +54,23 @@ def main(num_trials, num_selections, avoid_aem, num_arms_min, num_arms_max):
             input_state = generate_input(num_arms, method)
             # Average reward for this env.
             conv_result = np.array(Parallel(n_jobs=-1)([delayed(CMAB)(env, num_selections, input_state, method) for _ in range(num_trials)])).mean(axis=0)
+            np.save(dir_for_output+'{}M/conv/reward_env_{}.npy'.format(num_arms, i), conv_result)
             conv_results[i] = conv_result.mean()
             method = 'psm'
             input_state = generate_input(num_arms, method)
             psm_result = np.array(Parallel(n_jobs=-1)([delayed(CMAB)(env, num_selections, input_state, method) for _ in range(num_trials)])).mean(axis=0)
+            np.save(dir_for_output+'{}M/psm/reward_env_{}.npy'.format(num_arms, i), psm_result)
             psm_results[i] = psm_result.mean()
             method = 'ideal'
             input_state = generate_input(num_arms, method)
             ideal_result = np.array(Parallel(n_jobs=-1)([delayed(CMAB)(env, num_selections, input_state, method) for _ in range(num_trials)])).mean(axis=0)
+            np.save(dir_for_output+'{}M/ideal/reward_env_{}.npy'.format(num_arms, i), ideal_result)
             ideal_results[i] = ideal_result.mean()
             if not avoid_aem:
                 method = 'aem'
                 input_state = generate_input(num_arms, method)
                 aem_result = np.array(Parallel(n_jobs=-1)([delayed(CMAB)(env, num_selections, input_state, method) for _ in range(num_trials)])).mean(axis=0)
+                np.save(dir_for_output+'{}M/aem/reward_env_{}.npy'.format(num_arms, i), aem_result)
                 aem_results[i] = aem_result.mean()
         ave_convs.append(conv_results.mean())
         std_convs.append(conv_results.std())
@@ -69,15 +81,6 @@ def main(num_trials, num_selections, avoid_aem, num_arms_min, num_arms_max):
         if not avoid_aem:
             ave_aems.append(aem_results.mean())
             std_aems.append(aem_results.std())
-    now = datetime.datetime.now()
-    current_time = now.strftime("%Y-%m-%d-%H-%M-%S")
-    dir_for_output = "data/td_trial/" + current_time
-    os.makedirs(dir_for_output, exist_ok=False)
-    np.save(dir_for_output+'/conv_results.npy', conv_results)
-    np.save(dir_for_output+'/psm_results.npy', psm_results)
-    np.save(dir_for_output+'/ideal_results.npy', ideal_results)
-    if not avoid_aem:
-        np.save(dir_for_output+'/aem_results.npy', aem_results)
     # Draw figures.
     plt.figure(figsize=(8, 6), dpi=80)
     plt.rcParams['font.family'] = 'Times New Roman'
@@ -114,7 +117,7 @@ def main(num_trials, num_selections, avoid_aem, num_arms_min, num_arms_max):
         plt.xticks(x2, label_x)
     else:
         plt.xticks(x2+0.1, label_x)
-    plt.savefig(dir_for_output+'/average_reward.pdf', bbox_inches="tight", pad_inches=0.05)
+    plt.savefig(dir_for_output+'average_reward.pdf', bbox_inches="tight", pad_inches=0.05)
 
     plt.figure(figsize=(8, 6), dpi=80)
     if avoid_aem:
@@ -141,7 +144,7 @@ def main(num_trials, num_selections, avoid_aem, num_arms_min, num_arms_max):
         plt.xticks(x2, label_x)
     else:
         plt.xticks(x2+0.1, label_x)
-    plt.savefig(dir_for_output+'/std_reward.pdf', bbox_inches="tight", pad_inches=0.05)
+    plt.savefig(dir_for_output+'std_reward.pdf', bbox_inches="tight", pad_inches=0.05)
 
 
 def boolean_string(s):
@@ -155,7 +158,7 @@ if __name__ == "__main__":
     parser.add_argument("--num_trials", default=100, type=int, help='Number of CMAB trials.')
     parser.add_argument("--num_selections", default=1000, type=int, help='Number of selections in a CMAB trial.')
     parser.add_argument("--avoid_aem", default=True, type=boolean_string, help='Avoid AEM for reduction of time.')
-    parser.add_argument("--num_arms_min", default=4, type=int, help='Minimum number of machines.')
-    parser.add_argument("--num_arms_max", default=6, type=int, help='Maximum number of machines.')
+    parser.add_argument("--num_arms_min", default=3, type=int, help='Minimum number of machines.')
+    parser.add_argument("--num_arms_max", default=4, type=int, help='Maximum number of machines.')
     args = parser.parse_args()
-    main(args.num_trials, args.num_selections, args.avoid_aem, args.num_arms_min, args.num_arms_max)
+    main(args.num_trials, args.num_selecåtions, args.avoid_aem, args.num_arms_min, args.num_arms_max)
